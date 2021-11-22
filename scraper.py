f00d74fcf67579bd157f49b0e74c9ddc644bc30a
andree93 <34599817+andree93@users.noreply.github.com>
2021-11-22 19:48:29 +0100

    Add files via upload
    code cleanup

--- scraper.py
+++ scraper.py
@@ -2,14 +2,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-
-print("Con questo programma potrai salvare i link diretti agli episodi di vari shows presenti su archive.org\nSaranno estratti gli url diretti ai file riproducibili con il player HTML5 integrato nel sito\nTi basterà inserire l'URL alla pagina nel formato \"https://archive.org/details/ABC\"")
+print("With this script you can save all direct links to archive.org website audio and video streams\nAll playable links with the internal HTML5 player will be extracted and saved in a TXT file (one per line)\nYou must enter the webpage link in the format \"https://archive.org/details/ABC\"")
 URL = input("Please enter archive.org URL page: ")
 
 if not URL.startswith("https://archive.org/details/"):
     print("URL format error! Please try again: ")
     URL = input("Please enter archive.org URL page: ")
-
 
 page = requests.get(URL)
 if page.status_code == 200:
@@ -19,16 +17,12 @@ if page.status_code == 200:
     print("Trovati "+str(len(links))+" link")
     for link in links:
         link_txt += link['href']+"\n"
-    file_name = input("Nome del file dove salvare i link: ")
+    file_name = input("Please enter file name (where all links will be saved): ")
     percorso = os.path.join(os.getcwd(), file_name + ".txt")
     with open(percorso, "w", encoding='utf-8') as file:
         file.write(link_txt)
         print("File salvato in: "+percorso)
-        
 else:
     print("Request error! HTTP Error code: "+ str(page.status_code))
     print("Exiting...")
-
-
-
 
